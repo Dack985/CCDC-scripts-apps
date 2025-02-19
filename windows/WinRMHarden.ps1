0244ab448d2f582a8b70b73d7d7933bfda98cb68
Jonathan Edwards <76668213+SP1R1T-5@users.noreply.github.com>
2025-02-19 14:44:57 -0500

    Create WinRMHarden.ps1

---
+++ windows/WinRMHarden.ps1
@@ -0,0 +1,11 @@
+# Stop WinRM Service
+net stop WinRM
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WinRM\Service" /v AllowUnencryptedTraffic /t REG_DWORD /d 0 /f
+# Disable WinRM Client Digiest authentication
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WinRM\Client" /v AllowDigest /t REG_DWORD /d 0 /f
+# Disabling RPC usage from a remote asset interacting with scheduled tasks
+reg add "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Schedule" /v DisableRpcOverTcp /t REG_DWORD /d 1 /f
+# Disabling RPC usage from a remote asset interacting with services
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control" /v DisableRemoteScmEndpoints /t REG_DWORD /d 1 /f
+#Starts WinRM Service
+net start WinRM
